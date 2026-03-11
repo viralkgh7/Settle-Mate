8321c61aa96305fcc5f9ca8cce4e536cec64c1d0
Viral Kathiriya <202351062@iiitvadodara.ac.in>
2026-03-11 11:07:00 +0530

    chore: initial project setup and dependencies

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AppProvider } from './src/context/AppContext';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <AppProvider>
+        <AppNavigator />
+        <StatusBar style="auto" />
+      </AppProvider>
+    </SafeAreaProvider>
+  );
+}
